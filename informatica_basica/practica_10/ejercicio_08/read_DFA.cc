0f664c60aa94cda1ec034735b9c2656f20d531ac
RafaGomezGuillen <108660843+RafaGomezGuillen@users.noreply.github.com>
2022-07-04 11:43:26 +0100

    Add files via upload

---
+++ informatica_basica/practica_10/ejercicio_08/read_DFA.cc
@@ -0,0 +1,44 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Rafael Gomez Guilen alu0101462578
+ * @date 13/12/2021
+ * @brief Escriba un programa read_DFA que lea un fichero de texto input.dfa que
+ *        contiene la especificación de un DFA e imprima en pantalla las
+ *        características del mismo.
+ * @bug No hay bugs conocidos
+ * @see
+ * https://github.com/ULL-ESIT-IB-2021-2022/ib-2021-2022-p10_files-RafaelGomezGuillen
+ */
+
+#include "read_DFA.h"
+
+#include <cstdlib>  /// exit
+#include <fstream>  // For the file streams
+#include <iostream>
+using namespace std;
+
+/** Muestra el modo de uso correcto del programa
+ *  En caso de que el uso no sea el correcto, muestra el mensaje y finaliza
+ *  la ejecución del programa.
+ *  El programa precisa de un fichero para su ejecución.
+ *
+ *  @param[in] argc Número de parámetros de la línea de comando
+ *  @param[in] argv Vector que contiene (char *) fichero (read_DFA.txt)
+ */
+
+void Usage(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout << argv[0] << ": Falta un fichero de texto como parametro" << endl;
+    cout << "Pruebe " << argv[0] << " --help para más información" << endl;
+    exit(EXIT_FAILURE);
+  }
+  string parameter{argv[1]};
+  if (parameter == "--help") {
+    cout << kHelpText << endl;
+    exit(EXIT_FAILURE);
+  }
+}
