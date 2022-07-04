f67084fa1b1a4a213cfd138d61489befc503cb04
RafaGomezGuillen <108660843+RafaGomezGuillen@users.noreply.github.com>
2022-07-04 11:38:46 +0100

    Add files via upload

---
+++ informatica_basica/practica_10/ejercicio_04/delete_blank_lines.h
@@ -0,0 +1,22 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica
+ *
+ * @author Rafael Gomez Guilen alu0101462578
+ * @date 13/12/2021
+ * @brief Este programa declara la "Help Text" constante y una funcion
+ * @bug No hay bugs conocidos
+ * @see
+ * https://github.com/ULL-ESIT-IB-2021-2022/ib-2021-2022-p10_files-RafaelGomezGuillen
+ */
+
+#include <iostream>
+using namespace std;
+
+const string kHelpText =
+    " Este programa elimina de un fichero de texto las entradas de líneas en "
+    "blanco.";
+
+void Usage(int argc, char *argv[]);
